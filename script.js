e348139dc38c31a76fdea40c76d0b6b910172a09
Artur <elkin.as@mail.ru>
2018-08-01 10:17:00 +0300

    slider automated

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let imageSize = 512;
 let shiftAmount = 0;
+let shiftInterval;
 
-function buttonShiftLeft () {
+function shiftRight () {
 	let row = document.getElementById('row');
 	if(shiftAmount == -(imageSize*4)) {
 		shiftAmount = 0;
@@ -11,7 +12,7 @@ function buttonShiftLeft () {
 	row.style.left = shiftAmount + 'px';
 }
 
-function buttonShiftRight() {
+function shiftLeft () {
 	let row = document.getElementById('row');
 	if(shiftAmount == 0) {
 		shiftAmount = -imageSize*4;
@@ -21,5 +22,20 @@ function buttonShiftRight() {
 	row.style.left = shiftAmount + 'px';
 }
 
-document.getElementById('button-left').addEventListener('click', buttonShiftLeft);
-document.getElementById('button-right').addEventListener('click', buttonShiftRight);
+function shiftOn() {
+	shiftInterval = setInterval(shiftRight, 2200);
+	document.getElementById('on').style.display = 'none';
+	document.getElementById('off').style.display = 'block';
+}
+
+function shiftOff() {
+	clearInterval(shiftInterval);
+	document.getElementById('off').style.display = 'none';
+	document.getElementById('on').style.display = 'block';
+}
+
+
+document.getElementById('button-right').addEventListener('click', shiftRight);
+document.getElementById('button-left').addEventListener('click', shiftLeft);
+document.getElementById('on').addEventListener('click', shiftOn);
+document.getElementById('off').addEventListener('click', shiftOff);
